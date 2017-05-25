2c1e580bc93d11136c0fbc572a6a972be6861b28
Lu Yi <435977170@qq.com>
2017-05-25 20:33:39 +0800

    luyi at 6011 upload in 2017/05/25

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # some_companies_test
-17年几个互联网公司的笔试题目
+***
+## **介绍**
+17年几个互联网公司的笔试题目，主要会来自[牛客网](https://www.nowcoder.com/)，[赛码网](http://www.acmcoder.com/)上的历年公司例题，同时也包含了各自平台上的模拟测试；
+***
+## **内容**
+### **赛码网模拟**
+- [3月17号](http://exercise.acmcoder.com/comp_test?paperId=211) >> acmcoder_2017_1
+- [3月31号](http://exercise.acmcoder.com/comp_test?paperId=232) >> acmcoder_2017_2
+
+### **牛客网模拟**
+- [模拟第一场](https://www.nowcoder.com/test/4236887/summary) >> nowcode_2017_1
+- [模拟第二场](https://www.nowcoder.com/test/4546329/summary) >> nowcode_2017_2
+- [模拟第三场](https://www.nowcoder.com/test/5217106/summary) >> nowcode_2017_3
+
+### **好未来2017秋招笔试汇总**
+- [好未来](https://www.nowcoder.com/test/3910507/summary) >> haoweilai
+***
+## **备注**
+相应的解题思路会放在代码里面作为注释注解
--- acmcoder_2017_2/trans_mars.cpp
+++ acmcoder_2017_2/trans_mars.cpp
@@ -5,56 +5,48 @@
 
 using namespace std;
 
-string process(string& s, unordered_map<string, string>& table){
+void process(string& s, unordered_map<string, string>& table){
     int start = 0, end = 0;
     int lens = s.size();
-    bool is_word = true, is_split = false;
+    string tmp;
     for(int i=0;i<lens;i++){
         if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')){
-            if(is_word){
-                is_split = false;
-            }
-            else{
-                start = i;
-                is_word = !is_word;
-            }
+            tmp.push_back(s[i]);
         }
         else{
-            if(is_split){
-                is_word = false;
-                cout << s[i];
-            }
+            if(tmp.empty()) cout << s[i];
             else{
-                if(table.find(s.substr(start, i - start))!=table.end()) cout << table[s.substr(start, i - start)];
-                else    cout << s.substr(start, i - start);
-                is_split = !is_split;
+                if(table.find(tmp)!=table.end())    cout << table[tmp] << s[i];
+                else    cout << tmp << s[i];
+                tmp.clear();
             }
         }
+    }
+    if(!tmp.empty()){
+        if(table.find(tmp)!=table.end())    cout << table[tmp];
+        else    cout << tmp;
     }
     cout << endl;
 }
 
 int main(){
-    bool is_first = true;
     unordered_map<string, string> table;
     string str;
     while(getline(cin, str)){
-        if(str=='START'&&is_first)  continue;
-        else if(str=='END') break;
+        if(str=="START")  continue;
+        else if(str=="END") break;
         else{
             int pos = str.find(' ');
-            string first = str.substr(0,pos), second = str.substr(pos);
+            string first = str.substr(0,pos), second = str.substr(pos+1);
             table[second] = first;
         }
     }
-    string unsolved;
     while(getline(cin, str)){
-        if(str=='START'&&is_first)  continue;
-        else if(str=='END') break;
+        if(str=="START")  continue;
+        else if(str=="END") break;
         else{
-            unsolved = str;
+            process(str, table);
         }
     }
-    process(unsolved, table);
     return 0;
 }
